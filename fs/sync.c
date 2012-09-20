e893bc5cd9f4a4e9e5d03047b20b8ad73b4c29e1
thicklizard <thicklizard@gmail.com>
2012-09-20 17:11:51 -0500

    add_a_few_gov_tweak_mpdecision

--- fs/sync.c
+++ fs/sync.c
@@ -139,8 +139,6 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 	int ret;
 	int fput_needed;
 
-return 0;
-
 	file = fget_light(fd, &fput_needed);
 	if (!file)
 		return -EBADF;
@@ -169,8 +167,6 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
 	struct address_space *mapping = file->f_mapping;
 	int err, ret;
-
-return 0;
 
 	if (!file->f_op || !file->f_op->fsync) {
 		ret = -EINVAL;
@@ -204,8 +200,6 @@ EXPORT_SYMBOL(vfs_fsync_range);
  */
 int vfs_fsync(struct file *file, int datasync)
 {
-
-return 0;
 	return vfs_fsync_range(file, 0, LLONG_MAX, datasync);
 }
 EXPORT_SYMBOL(vfs_fsync);
@@ -214,8 +208,6 @@ static int do_fsync(unsigned int fd, int datasync)
 {
 	struct file *file;
 	int ret = -EBADF;
-
-return 0;
 
 	file = fget(fd);
 	if (file) {
@@ -227,17 +219,13 @@ return 0;
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
-return 0;
 	return do_fsync(fd, 0);
 }
 
-
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
-return 0;
 	return do_fsync(fd, 1);
 }
-
 
 /**
  * generic_write_sync - perform syncing after a write if file / inode is sync
@@ -249,8 +237,6 @@ return 0;
  */
 int generic_write_sync(struct file *file, loff_t pos, loff_t count)
 {
-return 0;
-
 	if (!(file->f_flags & O_DSYNC) && !IS_SYNC(file->f_mapping->host))
 		return 0;
 	return vfs_fsync_range(file, pos, pos + count - 1,
@@ -314,8 +300,6 @@ SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 	loff_t endbyte;			/* inclusive */
 	int fput_needed;
 	umode_t i_mode;
-
-return 0;
 
 	ret = -EINVAL;
 	if (flags & ~VALID_FLAGS)
