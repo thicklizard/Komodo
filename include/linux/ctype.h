8a75b39eb4ce6534b278270cf0ec8a17846fc209
ca1ne <ca1nexda@gmail.com>
2012-09-20 22:50:32 -0400

    [PATCH] lib/vsprintf: replace link to Draft by final RFC number, lib: make _tolower() public, lib/kstrtox: common code, between kstrto*() and simple_strto*() functions

--- include/linux/ctype.h
+++ include/linux/ctype.h
@@ -52,4 +52,12 @@ static inline unsigned char __toupper(unsigned char c)
 #define tolower(c) __tolower(c)
 #define toupper(c) __toupper(c)
 
+/*
+ * Fast implementation of tolower() for internal usage. Do not use in your
+ */
+static inline char _tolower(const char c)
+{
+	return c | 0x20;
+}
+
 #endif
