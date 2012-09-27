74399e69de73ffa85ceb42c19297a4cc51dfbc1a
thicklizard <thicklizard@gmail.com>
2012-09-27 14:08:21 -0500

    Up_gpu_freq

--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -2539,7 +2539,7 @@ static struct resource kgsl_3d0_resources[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 {
-.gpu_freq = 320000000,
+.gpu_freq = 484000000,
 .bus_freq = 4,
 .io_fraction = 0,
 },
@@ -2616,7 +2616,7 @@ static struct resource kgsl_2d0_resources[] = {
 static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 	.pwrlevel = {
 		{
-.gpu_freq = 266667000,
+.gpu_freq = 300000000,
 .bus_freq = 2,
 },
 {
@@ -2683,7 +2683,7 @@ static struct resource kgsl_2d1_resources[] = {
 static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 	.pwrlevel = {
 	{
-.gpu_freq = 266667000,
+.gpu_freq = 300000000,
 .bus_freq = 2,
 },
 {
